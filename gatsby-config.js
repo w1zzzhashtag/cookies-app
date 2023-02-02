eb1d8183cfc9858ea5acbf62c6caf8a6c3e438cb
v.kuzmin <vladislav.kuzmin@nordclan.com>
2023-02-02 21:23:00 +0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `cook-app`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
